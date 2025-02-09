f4271618e204b0efc01bf2e3cb8d6a0c85e46ac7
Alex <askow2015@gmail.com>
2025-02-08 17:19:49 -0700

    Added New List Functionality through new form creation

--- todo.rb
+++ todo.rb
@@ -2,15 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
 
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    {name: "Lunch Grociers", todos: [] },
-    {name: "Dinner Grociers", todos: []}
-  ]
-
+  @lists = session[:lists]
   erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect "/lists"
 end
