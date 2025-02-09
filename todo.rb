de4a121484f55a7d28eeb93e17c7cbd77553d7de
Alex <askow2015@gmail.com>
2025-02-08 18:45:03 -0700

    Refactored the logic branch for creating a new list

--- todo.rb
+++ todo.rb
@@ -19,8 +19,8 @@ helpers do
   end
 end
 
-def good_size?(input)
-  (1..100).cover? input.size
+def bad_size?(input)
+  !(1..100).cover? input.size
 end
 
 def existing_list?(input)
@@ -47,15 +47,17 @@ end
 post "/lists" do
   list_name = params[:list_name].strip
 
-  if good_size?(list_name) && !existing_list?(list_name)
-    session[:lists] << {name: list_name , todos: []}
-    session[:success] = "The list has been created."
-    redirect "/lists"
+  if bad_size?(list_name)
+    session[:error] = "The list name must be between 1 and 100 characters."
+    erb :new_list, layout: :layout
+
   elsif existing_list?(list_name)
     session[:error] = "The list name must be unique."
     erb :new_list, layout: :layout
-  else 
-    session[:error] = "The list name must be between 1 and 100 characters."
-    erb :new_list, layout: :layout
+    
+  else  
+    session[:lists] << {name: list_name , todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
   end
 end
