60e625de76fc407af40f3d434c9b6d7cd8f30681
Alex <askow2015@gmail.com>
2025-02-08 19:32:19 -0700

    Satisfied Rubocop Format Complaints

--- todo.rb
+++ todo.rb
@@ -1,6 +1,8 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubi"
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubi'
 
 configure do
   enable :sessions
@@ -9,13 +11,13 @@ end
 
 helpers do
   def flash_message(name)
-    if session[name]
-      <<~MESSAGE
+    return unless session[name]
+
+    <<~MESSAGE
       <div class = "flash #{name}">
-        <p>#{session.delete(name)}</p>
+      <p>#{session.delete(name)}</p>
       </div>
-      MESSAGE
-    end
+    MESSAGE
   end
 end
 
@@ -23,16 +25,16 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list
 end
 
@@ -41,28 +43,28 @@ def invalid_size?(input)
 end
 
 def existing_name?(input)
-  session[:lists].any? { |list| list[:name] == input } 
+  session[:lists].any? { |list| list[:name] == input }
 end
 
 def determine_error(input)
   if invalid_size?(input)
-    session[:error] = "The list name must be between 1 and 100 characters."
+    session[:error] = 'The list name must be between 1 and 100 characters.'
 
   elsif existing_name?(input)
-    session[:error] = "The list name must be unique."
+    session[:error] = 'The list name must be unique.'
   end
 end
 
-post "/lists" do
+post '/lists' do
   list_name = params[:list_name].strip
   determine_error(list_name)
 
   if session[:error]
     erb :new_list, layout: :layout
-    
-  else  
-    session[:lists] << {name: list_name , todos: []}
-    session[:success] = "The list has been created."
-    redirect "/lists"
+
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
   end
 end
